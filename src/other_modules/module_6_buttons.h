8480fd78baf19d745636db769edd162e8b76d24d
salvaxtraw5337 <epsuv5337@gmail.com>
2026-03-14 21:38:22 -0600

    created module_6_buttons.h

---
+++ src/other_modules/module_6_buttons.h
@@ -0,0 +1,27 @@
+/*
+Version: 1.0.0
+Autor: Estrada Pulido Salvador
+Fecha: 14/03/2026
+
+Descripcion:
+El siguiente código, el cual es meramente un llamado de funciones para operar como libreria, se compone de 3 secciones:
+* Definiciones - Le da nombre a la libreria y comienza el llamado
+* bool {funcion}() - Se llaman a las funciones del codigo en el que se basa
+* #endif - Cierra el llamado de funciones
+
+/* 
+MOD: Esta sección se usará para especificar las modificaciones que sufra el código.
+*/
+
+// Seccion Primera
+#ifndef botones_h
+#define botones_h
+
+// Seccion Segunda
+bool enter();
+bool up_button();
+bool down_button();
+void botonesInit();
+
+// Seccion Tercera
+#endif
